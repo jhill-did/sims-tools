9343b3aba8a5ad8760eadc877eeca613df2c49c9
Jordan Hill <jordan.ri.hill@gmail.com>
2023-07-23 16:52:28 -0500

    Adds hash routing.

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -4,56 +4,60 @@ import React from 'react';
 import { NormalConverter } from './NormalConverter';
 import { ModelViewer } from './ModelViewer';
 import { Tooltip } from './Tooltip';
+import { Routes, Route } from 'react-router';
+import { HashRouter, Link } from 'react-router-dom';
 
 type Mode = 'normal-converter' | 'model-viewer';
 
 const App = () => {
-  const [mode, setMode] = useState<Mode>('model-viewer');
-
   return (
-    <div
-      style={{
-        flexDirection: 'row',
-        position: 'absolute',
-        display: 'flex',
-        boxSizing: 'border-box',
-        width: '100%',
-        height: '100%',
-        flexWrap: 'nowrap',
-        gap: '16px',
-        alignItems: 'center',
-      }}
-    >
+    <HashRouter>
       <div
         style={{
+          flexDirection: 'row',
+          position: 'absolute',
           display: 'flex',
           boxSizing: 'border-box',
-          padding: '6px',
-          flexDirection: 'column',
-          backgroundColor: '#222',
+          width: '100%',
           height: '100%',
-          width: '36px',
-          gap: '6px',
+          flexWrap: 'nowrap',
+          gap: '16px',
+          alignItems: 'center',
         }}
       >
-        <Tooltip text="Model Viewer">
-          <button className="app-icon-button" onClick={() => { setMode('model-viewer'); }}>
-            <img src="3d-rotate.svg" style={{ filter: 'invert(0.8)' }} />
-          </button>
-        </Tooltip>
-        
-        <Tooltip text="Normal Converter">
-          <button className="app-icon-button" onClick={() => { setMode('normal-converter'); }}>
-            <img src="arrow-up-bar.svg" style={{ filter: 'invert(0.8)' }} />
-          </button>
-        </Tooltip>
-      </div>
+        <div
+          style={{
+            display: 'flex',
+            boxSizing: 'border-box',
+            padding: '6px',
+            flexDirection: 'column',
+            backgroundColor: '#222',
+            height: '100%',
+            width: '36px',
+            gap: '6px',
+          }}
+        >
+          <Tooltip text="Model Viewer">
+            <Link to="/" className="app-icon-button">
+              <img src="3d-rotate.svg" style={{ filter: 'invert(0.8)' }} />
+            </Link>
+          </Tooltip>
+          
+          <Tooltip text="Normal Converter">
+            <Link to="/normal-converter" className="app-icon-button">
+              <img src="arrow-up-bar.svg" style={{ filter: 'invert(0.8)' }} />
+            </Link>
+          </Tooltip>
+        </div>
 
-      <div style={{ display: 'flex', flexGrow: 1, justifyContent: 'center' }}>
-        {mode === 'model-viewer' && <ModelViewer />}
-        {mode === 'normal-converter' && <NormalConverter />}
+        <div style={{ display: 'flex', flexGrow: 1, justifyContent: 'center' }}>
+            <Routes>
+              <Route path="/" Component={ModelViewer} />
+              <Route path="/normal-converter" Component={NormalConverter} />
+            </Routes>
+        </div>
       </div>
-    </div>
+    </HashRouter >
   );
 };
 
